2d98b7918b564c76feabab109dca190e8330b702
Musab Guma'a <u2104643@live.warwick.ac.uk>
2022-09-30 21:10:06 +0100

    Minor Changes

--- src/Interface.hs
+++ src/Interface.hs
@@ -1,12 +1,12 @@
 module Interface (repl) where
 
--- import Control.Monad            ( forever )
+import Control.Monad            ( forever )
 import System.IO
 
 import qualified Lib as L
 
 repl :: IO ()
-repl = putStr brainfuckInfo >> L.repl
+repl = putStr brainfuckInfo >> forever L.repl
 
 brainfuckInfo :: String
 brainfuckInfo = "\nBrainf**ck is an esoteric programming language which models a Turing Machine\n"
